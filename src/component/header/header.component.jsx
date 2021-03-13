bd305b22d79a0bf2137f22226bfe9f7efd6de2dc
[badrri-narayanan] <badrri1995@gmail.com>
2021-03-13 22:21:27 +0530

    added cart component and redux method to show/hide cart

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
+import CartIcon from '../cart-icon/cart-icon.component'
 
 import {auth} from '../../firebase/firebase.utils';
 
 import {ReactComponent as Logo} from '../../asset/crown.svg'
 import './header.styles.scss'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className="header">
         <Link to="/" className="logo-container">
             <Logo className="logo" />
@@ -21,12 +23,19 @@ const Header = ({currentUser}) => (
                     :
                     <Link className="option" to="/signin" >SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden ?
+                null:
+                <CartDropdown />
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser,
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden,
 })
 
 export default connect(mapStateToProps)(Header);
