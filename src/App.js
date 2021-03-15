11e802caa11ec95444ebed83ed26ce4a28b44c2b
[badrri-narayanan] <badrri1995@gmail.com>
2021-03-15 10:57:44 +0530

    added checkout page and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import {Route, Switch, Redirect} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './component/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import {createStructuredSelector} from 'reselect';
 
 import {connect} from 'react-redux'
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 import {setCurrentUser} from './redux/user/user.actions'
+import { selectCurrentUser } from './redux/user/user.selectors';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 class App extends React.Component {
 
@@ -51,13 +54,14 @@ class App extends React.Component {
                   <SignInAndSignUpPage/>
             } />
         </Switch>
+        <Route exact path="/checkout" component={CheckoutPage} />
       </div>
     );
   }
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser : user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser : selectCurrentUser,
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -9,6 +9,11 @@ import {ReactComponent as Logo} from '../../asset/crown.svg'
 import './header.styles.scss'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
+import { createStructuredSelector } from 'reselect';
+
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+
 const Header = ({currentUser, hidden}) => (
     <div className="header">
         <Link to="/" className="logo-container">
@@ -33,9 +38,9 @@ const Header = ({currentUser, hidden}) => (
     </div>
 )
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser,
-    hidden,
+const mapStateToProps = state => createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden,
 })
 
 export default connect(mapStateToProps)(Header);
